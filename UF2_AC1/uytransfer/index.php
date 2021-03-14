01b6aec779b3bc3f8117371b66fa0d743371a50e
GuillemCM <guillem.cusachs@mataro.epiaedu.cat>
2021-03-14 19:34:58 +0100

    Estructura pagina index.php acabada, faltan enlaces header

--- UF2_AC1/uytransfer/index.php
+++ UF2_AC1/uytransfer/index.php
@@ -14,22 +14,42 @@
     <script src="https://kit.fontawesome.com/ef14336b18.js" crossorigin="anonymous"></script>
   </head>
   <body>
-    <div class="container d-flex p-3 mx-auto bg-secondary">
-        <header class="mb-auto">
-            <div class="inner">
-                <h3 class="m-1">Uy!Transfer</h3>
+    <div class="container p-3 bg-secondary mt-1" style="border-style: solid;">
+        <div class="row align-items-center">
+            <div class="col-8">
+                <h3 class="text-start">Uy!Transfer</h3>
             </div>
-        </header>
+            <div class="col-4 text-center">
+                <p class="pt-1"><a style="color: black;" href="">Enviar archivo</a> | <a style="color: black;" href="">Mis últimos archivos</a></p> 
+            </div>
+        </div>
     </div>
+
     <div class="container">
         <div class="row">
-            <div class="offset-2 col-8 justify-content-center mt-4">
+            <div class="offset-2 col-8 justify-content-center align-items-center mt-4">
                 <form name="file" action="upload.php" method="post" enctype="multipart/form-data">
                   <div class="m-3">
                     <input type="text" class="form-control" id="nameInput" placeholder="Tu nombre">
                   </div>
                   <div class="m-3">
-                    <input type="file" name="file" class="form-control form-control-sm" id="formFile"/>
+                    <input class="form-control" type="file" id="formFile">
+                  </div>
+                  <div class="m-3 form-check form-check-inline">
+                      <input class="form-check-input" type="checkbox" value="" id="enviarPorEmail">
+                      <label class="form-check-label" for="enviarPorEmail">
+                        Quiero enviar el link de descarga por email
+                      </label>
+                  </div>
+                  <div class="m-3">
+                      <input type="email" class="form-control" id="emailAEnviar" aria-describedby="email" placeholder="Email del destinatario">
+                  </div>
+                  <div class="m-3">
+                      <label>Mensaje</label>
+                      <textarea class="form-control rounded-0" id="mensajeEmail" rows="5"></textarea>
+                  </div>
+                  <div class="m-3 float-right">
+                      <button type="button" class="btn btn-light" style="border-color: black">Subir archivo</button>
                   </div>
                 </form>
             </div>
