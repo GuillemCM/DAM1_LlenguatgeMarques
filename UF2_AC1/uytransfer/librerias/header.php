b2fa40581a86469613446ecc21bd62c1c4804650
GuillemCM <guillem.cusachs@mataro.epiaedu.cat>
2021-03-14 20:29:54 +0100

    Solucionado problema al cargar las librerias

---
+++ UF2_AC1/uytransfer/librerias/header.php
@@ -0,0 +1,12 @@
+<?php
+	echo"<div class=\"container p-3 bg-secondary mt-1\" style=\"border-style: solid;\">
+        <div class=\"row align-items-center\">
+            <div class=\"col-8\">
+                <h3 class=\"text-start\">Uy!Transfer</h3>
+            </div>
+            <div class=\"col-4 text-center\">
+                <p class=\"pt-1\"><a style=\"color: black;\" href=\"index.php\">Enviar archivo</a> | <a style=\"color: black;\" href=\"index.php\">Mis últimos archivos</a></p> 
+            </div>
+        </div>
+    </div>";
+?>
